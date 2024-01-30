bcc1946032c205551cd4271b577eba8485aa81ce
Adrian Klausen <adrian.rosenborg@gmail.com>
2024-01-30 09:16:40 +0100

    Setups
    Some Setups to the project

--- Assets/Scripts/World Objects/WorldObjectManager.cs
+++ Assets/Scripts/World Objects/WorldObjectManager.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditor.Progress;
 
 public class WorldObjectManager : Singleton<WorldObjectManager>
 {
