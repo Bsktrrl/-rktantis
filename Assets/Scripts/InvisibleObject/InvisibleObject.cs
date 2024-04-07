ce288066946b9964f72a951ecef994c983e637aa
Adrian Klausen <adrian.rosenborg@gmail.com>
2024-04-08 00:38:04 +0200

    InvisibleObject - Small changes

--- Assets/Scripts/InvisibleObject/InvisibleObject.cs
+++ Assets/Scripts/InvisibleObject/InvisibleObject.cs
@@ -1,13 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InvisibleObject : MonoBehaviour
 {
-    [SerializeField] MeshRenderer renderer;
-    //[SerializeField] SphereCollider sphereCollider;
-
-    [SerializeField] float transparencyValue = new float();
+    [SerializeField] float transparencyValue = 1;
     [SerializeField] float distance;
+
+    [SerializeField] Collider objectCollider;
+    [SerializeField] List<GameObject> objectPartsList = new List<GameObject>();
+    [SerializeField] List<Material> materialList = new List<Material>();
+
     SphereCollider sphereCollider;
+    GameObject tempCollider;
 
 
     //--------------------
@@ -15,7 +21,7 @@ public class InvisibleObject : MonoBehaviour
 
     private void Start()
     {
-        //Add a SphereCollider to thismeObject
+        //Add a SphereCollider to this GameObject
         sphereCollider = gameObject.AddComponent<SphereCollider>();
         sphereCollider.radius = 5f;
         sphereCollider.isTrigger = true;
@@ -25,8 +31,43 @@ public class InvisibleObject : MonoBehaviour
     }
     private void Update()
     {
-        renderer.material.SetFloat("_Transparency", transparencyValue);
+        if (HotbarManager.Instance.selectedItem == Items.Flashlight)
+        {
+            for (int i = 0; i < materialList.Count; i++)
+            {
+                materialList[i].SetFloat("_Transparency", transparencyValue);
+            }
+        }
+        else
+        {
+            transparencyValue = 1;
+
+            for (int i = 0; i < materialList.Count; i++)
+            {
+                materialList[i].SetFloat("_Transparency", transparencyValue);
+            }
+        }
+
+        if (transparencyValue >= 1)
+        {
+            objectCollider.enabled = false;
+
+            for (int i = 0; i < objectPartsList.Count; i++)
+            {
+                objectPartsList[i].SetActive(false);
+            }
+        }
+        else
+        {
+            objectCollider.enabled = true;
+
+            for (int i = 0; i < objectPartsList.Count; i++)
+            {
+                objectPartsList[i].SetActive(true);
+            }
+        }
     }
+
 
     //--------------------
 
@@ -35,7 +76,7 @@ public class InvisibleObject : MonoBehaviour
     {
         if (other.gameObject.CompareTag("InvisibleTriggerPoint"))
         {
-            print("OnTriggerEnter");
+            tempCollider = other.gameObject;
 
             distance = Vector3.Distance(transform.position, other.gameObject.transform.position);
 
@@ -47,7 +88,8 @@ public class InvisibleObject : MonoBehaviour
     {
         if (other.gameObject.CompareTag("InvisibleTriggerPoint"))
         {
-            print("OnTriggerStay");
+            tempCollider = other.gameObject;
+
             distance = Vector3.Distance(transform.position, other.gameObject.transform.position);
 
             transparencyValue = (distance / sphereCollider.radius);
@@ -58,7 +100,8 @@ public class InvisibleObject : MonoBehaviour
     {
         if (other.gameObject.CompareTag("InvisibleTriggerPoint"))
         {
-            print("OnTriggerExit");
+            tempCollider = null;
+
             distance = 0;
             transparencyValue = 1;
         }
