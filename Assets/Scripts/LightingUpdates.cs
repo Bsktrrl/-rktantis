8d83fb95c5385291f277f2e69cb1c4eee8512954
Hattematt <mathias.kapelrud@gmail.com>
2024-04-24 16:51:04 +0200

    Map fixes
    -Fixed collision to floor above Arídea entrance
    -Added light and ruins outside of Arídea entrance

--- Assets/Scripts/LightingUpdates.cs
+++ Assets/Scripts/LightingUpdates.cs
@@ -6,6 +6,7 @@ public class LightingUpdates : MonoBehaviour
 {
     public Light mainLight;
     public Light secondaryLight;
+    float fogDarkness;
 
     void Start()
     {
@@ -15,10 +16,18 @@ public class LightingUpdates : MonoBehaviour
     IEnumerator UpdateLighting()
     {
         yield return new WaitForSeconds(0.1f);
+        
+        //Sun and moon
         secondaryLight.transform.forward = -mainLight.transform.forward;
         mainLight.intensity = Mathf.Clamp(mainLight.transform.forward.y, -1, -0.05f) * -1;
-        RenderSettings.fogColor = new Color(1, 0.8156863f, 0.6078432f) * ((1 - mainLight.transform.forward.y) * 0.5f);
+        
+        //Fog
+        fogDarkness = (1 - mainLight.transform.forward.y) * 0.5f;
+        RenderSettings.fogColor = new Color(1 * fogDarkness, 0.8156863f * fogDarkness, 0.6078432f * fogDarkness, 1);
+
+        //Intensity of ambient light
         RenderSettings.ambientIntensity = Mathf.Pow(Mathf.Clamp(mainLight.transform.forward.y, 0, 1) + 1, 2f);
+
         DynamicGI.UpdateEnvironment();
         yield return new WaitForSeconds(1);
         StartCoroutine(UpdateLighting());
