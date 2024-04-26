80f851d6a22310c44fdb1ef3ac78eacfce58e885
Hattematt <mathias.kapelrud@gmail.com>
2024-04-26 02:34:22 +0200

    Updated lighting and fog at night
    -Updated lighting and fog at night

--- Assets/Scripts/LightingUpdates.cs
+++ Assets/Scripts/LightingUpdates.cs
@@ -25,15 +25,18 @@ public class LightingUpdates : MonoBehaviour
         //Fog
         fogDarkness = (1 - mainLight.transform.forward.y) * 0.5f;
         RenderSettings.fogColor = new Color(1 * fogDarkness, 0.8156863f * fogDarkness, 0.6078432f * fogDarkness, 1);
+        //RenderSettings.fogDensity = 0.0025f * ((1 + mainLight.transform.forward.y) * 2 + 1);
+        RenderSettings.fogDensity = Mathf.Clamp(mainLight.transform.forward.y, 0, 1) * 0.0075f + 0.0025f;
 
         //Intensity of ambient light
-        RenderSettings.ambientIntensity = Mathf.Pow(Mathf.Clamp(mainLight.transform.forward.y, 0, 1) + 1, 2f);
+        RenderSettings.ambientIntensity = Mathf.Pow(((Mathf.Clamp(mainLight.transform.forward.y, -0.25f, 1) + 0.25f) * 0.8f), 3) * 5 + 1;
+        print(RenderSettings.ambientIntensity);
 
         if (Vector3.Dot(Vector3.Normalize(player.transform.position), mainLight.transform.forward) <= 0.9f)
         {
             DynamicGI.UpdateEnvironment();
         }
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         StartCoroutine(UpdateLighting());
     }
 }
