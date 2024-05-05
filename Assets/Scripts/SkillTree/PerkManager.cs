c8d8bd9e7c24752a3be2d8a3b20e89c971fef493
Adrian Klausen <adrian.rosenborg@gmail.com>
2024-05-05 03:31:35 +0200

    Preperation for making Perks

--- Assets/Scripts/SkillTree/PerkManager.cs
+++ Assets/Scripts/SkillTree/PerkManager.cs
@@ -7,22 +7,21 @@ using UnityEngine.UI;
 public class PerkManager : Singleton<PerkManager>
 {
     public PerkValues perkValues;
-    //public PerkActivations perkActivations;
 
 
     //--------------------
 
 
+    #region Save/Load
     public void LoadData()
     {
         perkValues = DataManager.Instance.perks_Store;
-        //perkActivations = DataManager.Instance.perkActivations_Store;
     }
     public void SaveData()
     {
         DataManager.Instance.perks_Store = perkValues;
-        //DataManager.Instance.perkActivations_Store = perkActivations;
     }
+    #endregion
 
 
     //--------------------
@@ -67,43 +66,9 @@ public class PerkValues
     [Header("Tools")]
 
 
-    [Header("Arídite")]
-
-
-    [Header("Ghost")]
+    [Header("Arídean")]
     //MovementReducer
     public float ghostMovementReducer_Right = 0;
     public float ghostMovementReducer_Up = 0;
     public float ghostMovementReducer_Speed = 0;
 }
-
-//[Serializable]
-//public class PerkActivations
-//{
-
-//    [Header("Player")]
-
-
-//    [Header("Inventory")]
-
-
-//    [Header("Tools")]
-
-
-//    [Header("Arídite")]
-
-
-//    [Header("Ghost")]
-//    //MovementReducer
-//    public bool ghost_T1_MovementReducer_Right = false;
-//    public bool ghost_T1_MovementReducer_Up = false;
-//    public bool ghost_T1_MovementReducer_Speed = false;
-
-//    public bool ghost_T2_MovementReducer_Right = false;
-//    public bool ghost_T2_MovementReducer_Up = false;
-//    public bool ghost_T2_MovementReducer_Speed = false;
-
-//    public bool ghost_T3_MovementReducer_Right = false;
-//    public bool ghost_T3_MovementReducer_Up = false;
-//    public bool ghost_T3_MovementReducer_Speed = false;
-//}
